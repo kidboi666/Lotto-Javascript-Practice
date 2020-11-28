be6e60d2f549c833586c99956f8b17c608be2bcf
kidboi666 <kidboi666@gmail.com>
2020-11-28 23:48:40 +0900

    자정 10분전에 업로드

---
+++ index.js
@@ -0,0 +1,13 @@
+// var numbers = Array(45);
+// var fill = numbers.fill();
+// var mapping = fill.map(function (element, index) {
+//   return index + 1;
+// });
+
+
+var numbers = Array(45)
+.fill()
+.map(function (element, index) {
+  return index + 1;
+})
+console.log(numbers)
