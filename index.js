970a7d20a5de92ffdf6fe4c884a5bf52b44aa678
kidboi666 <kidboi666@gmail.com>
2020-11-29 00:05:16 +0900

    숫자추첨까지

--- index.js
+++ index.js
@@ -18,3 +18,6 @@ while (numbers.length > 0) {
   shuffle.push(moveNumber);
 }
 console.log(shuffle);
+var bonus = shuffle[shuffle.length - 1];
+var answer = shuffle.slice(0, 6) // 0부터 6째 자리까지, 6은 포함안됨. 실제로는 5까지
+console.log('당첨숫자들 ' + answer + ', 보너스 ' + bonus)
